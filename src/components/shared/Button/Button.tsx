80aff31ba9b06d58ba862e3cbe09568b32b389ab
yujo <bedro27@gmail.com>
2021-06-15 21:21:33 +0900

    fix: Button children type 수정
    - JSX.Element -> React.ReactNode
    
    Co-authored-by: Danny Kim <bigsaigon333@users.noreply.github.com>

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,10 +1,10 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ReactNode, ButtonHTMLAttributes } from "react";
 import classnames from "classnames/bind";
 import styles from "./Button.module.scss";
 
 // TODO: Style을 동적으로 적용하기 위한 Props를 받을 떄 타입 정의를 어떻게 할지
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: JSX.Element;
+  children: ReactNode;
   type: "submit" | "button" | "reset";
 }
 
